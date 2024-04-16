c2c98d33a7cc799bb954d51d89cfb5e15c48db50
johnny <john.robertson88@yahoo.com>
2024-04-15 21:02:31 -0700

    Add a form to insert a new employee. Create a custom DAO method to order result by last name ascending. redirect back to main list page on completion of new employee

--- src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -31,6 +33,28 @@ public class EmployeeController {
         // add to the spring model
         theModel.addAttribute("employees", theEmployees);
 
-        return "list-employees";
+        return "employees/list-employees";
     }
+
+    // Add a mapping to add employee form
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model theModel){
+        // Create model attribute to bind form data
+        Employee theEmployee = new Employee();
+
+        theModel.addAttribute("employee", theEmployee);
+
+        return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee theEmployee){
+        // Save the employee
+        employeeService.save(theEmployee);
+
+        // Use a redirect to prevent duplicate submissions
+        return "redirect:/employees/list";
+    }
+
+
 }
